8f274e337615463edfc21434a7141663276d8429
Sergey <cydoor88@gmail.com>
2014-09-21 15:04:28 +0400

    RandoApp Initial commit
    Создание приложение RandoApp

---
+++ RandoPhoto/Program.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Util;
+using Dot42.Manifest;
+using SimpleIOC;
+using RandoPhoto.Views;
+
+namespace RandoPhoto
+{
+    [Application("RandoPhoto")]
+    public class Program : Application
+    {
+        public static CSimpleIoCContainer IoCContainer = new CSimpleIoCContainer();
+
+        private void Bootstrap()
+        {
+            IoCContainer.Register<IMainView, MainActivity>(null);
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        public override void OnConfigurationChanged(Android.Content.Res.Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+        }
+
+        public override void OnLowMemory()
+        {
+            base.OnLowMemory();
+        }
+
+        public override void OnTerminate()
+        {
+            base.OnTerminate();
+        }
+    }
+}
+
---
+++ RandoPhoto/Views/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.App;
+using Android.Os;
+using Android.Widget;
+using Dot42;
+using Dot42.Manifest;
+using RandoPhoto.Views;
+
+namespace RandoPhoto
+{
+    [Activity]
+    public class MainActivity : Activity, IMainView
+    {
+        protected override void OnCreate(Bundle savedInstance)
+        {
+            base.OnCreate(savedInstance);
+            SetContentView(R.Layouts.main_view);
+        }
+    }
+}
