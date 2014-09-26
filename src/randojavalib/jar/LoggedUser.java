2978603a65c43c8eaa8984918c28c354f8911e54
gossiks <alexeykazin@yandex.ru>
2014-09-26 14:17:17 +0400

    Update LoggedUser.java

--- src/randojavalib/jar/LoggedUser.java
+++ src/randojavalib/jar/LoggedUser.java
@@ -2,36 +2,7 @@ package randojavalib.jar;
 
 import randojavalib.jar.Interfaces.ILoggedUser;
 
-public  class LoggedUser implements ILoggedUser {
-
-	private String mUserID;
-	private String mUsername;
-	private String mEmail;
+public class LoggedUser extend User implements ILoggedUser {
 	
-	
-	@Override
-	public String GetUID() {
-		return mUserID;
-	}
-
-	@Override
-	public String GetName() {
-		return mUsername;
-	}
-
-	@Override
-	public void SetName(String userName) {
-		mUsername = userName;
-	}
-
-	@Override
-	public void SetId(String UserId) {
-		mUserID = UserId;
-	}
-
-	@Override
-	public void SetEmail(String email) {
-		mEmail = email;
-	}
-
 }
+
