d429af80d264958af18d3553e3b3d498f61b49b2
gossiks <alexeykazin@yandex.ru>
2014-10-09 00:22:43 +0400

    Remove duplicated m_user and m_userID

--- randojavalib/LoggedUser.java
+++ randojavalib/LoggedUser.java
@@ -6,23 +6,18 @@ import randojavalib.jar.UserInterfaces.ILoggedUser;
 public class LoggedUser extends User implements ILoggedUser {
 	
 
-	@SuppressWarnings("unused")
-	private String m_userID;
-	@SuppressWarnings("unused")
-	private String m_userName;
+
 
 	public LoggedUser(String userID, String userName, String userEmail) {
 		super(userID, userName);
-		m_userID = userID;
-		m_userName = userName;
 		m_userEmail = userEmail;
-		
+		 
 	}
 
 	private String m_userEmail;
 	
 	
-	
+	 @Override
 	public String GetUserEmail() {
 		return m_userEmail;
 	}
