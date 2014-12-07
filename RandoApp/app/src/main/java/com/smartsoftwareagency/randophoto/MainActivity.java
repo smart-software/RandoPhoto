562c939c1b276b0281919c9772d4259c10616531
Sergey <cydoor88@gmail.com>
2014-12-08 01:41:41 +0300

    Added Java program

---
+++ RandoApp/app/src/main/java/com/smartsoftwareagency/randophoto/MainActivity.java
@@ -0,0 +1,53 @@
+package com.smartsoftwareagency.randophoto;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.rando.library.usermanager.UserInterfaces;
+import com.smartsoftwareagency.randophoto.ObjectFactory;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        UserInterfaces.IUserManager userManager = ObjectFactory.getUserManager();
+        UserInterfaces.ILoggedUser loggedUser = userManager.GetCurrentUser();
+
+        if(loggedUser != null) {
+            this.setMainView();
+        }
+        else {
+            // To Do: Go to login page
+        }
+    }
+
+    private void setMainView() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ RandoApp/app/src/main/java/com/smartsoftwareagency/randophoto/ObjectFactory.java
@@ -0,0 +1,27 @@
+package com.smartsoftwareagency.randophoto;
+
+import com.rando.library.usermanager.UserInterfaces.IUserManager;
+import com.rando.library.usermanager.UserManager;
+import com.rando.library.randomanager.IRandoManagerInterfaces.IRandoManager;
+import com.rando.library.randomanager.RandoManager;
+
+/**
+ * Created by SERGant on 08.12.2014.
+ */
+public final class ObjectFactory {
+    private static IUserManager m_userManager= null;
+    private static IRandoManager m_randoManager = null;
+
+    private ObjectFactory() {
+    }
+
+    public static IUserManager getUserManager() {
+        if(m_userManager == null) m_userManager = new UserManager();
+        return m_userManager;
+    }
+
+    public static IRandoManager getRandoManager() {
+        if(m_randoManager == null) m_randoManager = new RandoManager();
+        return m_randoManager;
+    }
+}
