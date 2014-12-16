f1f17ef8d1167c551e34213674166c0d0d5d5c14
gossiks <alexeykazin@yandex.ru>
2014-12-16 20:00:54 +0400

    Big update on cloud structure.
    1. From now on each new user  receive index number. First registered
    user is 1, second is 2 and so on.
    2. Pick2RandomUsers now much faster (now inside functions, will extract
    later).
    3. Cloud function "like" now have protection from  using from
    non-reviewers. Sends push to new reviewers, adds new reviewers. Now have
    _synchronous_  queries run simultaneously- much faster.

--- RandoCloud/cloud/main.js
+++ RandoCloud/cloud/main.js
@@ -7,139 +7,193 @@
 });
 */
 
-Parse.Cloud.define("pick2RandomUsers", function(request, response){
-	var queryCount = new Parse.Query(Parse.User);
-	queryCount.ascending("createdAt");
-	queryCount.notContainedIn("id", [request.user.id]);
-	queryCount.count({
-	  success: function(count) {
-		    // The count request succeeded. Pick two random numbers
-		  	var min = 0;
-		  	var max = count;
-		    var userIndex1 = Math.floor(Math.random() * (max - min + 1)) + min;
-		    var userIndex2 = 1;
-		    for (var i = 0; i < 100; i++) {
-				userIndex2 = Math.floor(Math.random() * (max - min + 1)) + min;
-				if (userIndex1 != userIndex2) {
+Parse.Cloud.define("pick2RandomUsersSimple", function(request, response){
+
+	var query = new Parse.Query("holder");
+	query.get("tsaSJggSgZ", { //This is holder for last user number
+		  success: function(holder) {
+			var min = 1; //for further improvements
+		    var userCount =  holder.get("userCount");
+			var max = userCount;
+		    var userCount1 = Math.floor(Math.random() * (max - min + 1)) + min;
+		    var userCount2 = 1; // initialized for crash protection. TODO - deprecate
+		    for (var i = 0; i < 100; i++) { //TODO random using [0;n) (n;max] where n  - is userCount1
+				userCount2 = Math.floor(Math.random() * (max - min + 1)) + min;
+				if (userCount1 != userCount2) {
 					break;
 				}
 			}
-		    
-		    queryCount.skip(userIndex1-1);
-		    queryCount.limit(1);
-		    queryCount.find({
-		    	  success: function(users) {
-		    		  	  var userId1 = new String();
-		    		      userId1 = users[0].id;
-		    		      queryCount.skip(userIndex2-1);
-		    		      queryCount.find({
-		    		    	  success: function(users) {
-		    		    		  var userId2 = new String();
-		    		    		  userId2 = users[0].id;
-		    		    		  var myResponse = { "userId1": userId1, "userId2": userId2};
-		    		    		  response.success(myResponse);
-		    		    	  }
-		    		    	});
-		    		  }
-		    		});
-		    
-		    
-		    
-		  },
-		  error: function(error) {
-		    // The request failed
-		  }
-		});
+		    var userArray =[userCount1,userCount2];
+		    response.success(userArray); 
+			}
+	});
 });
 
-Parse.Cloud.define("likePhoto", function(request, response) {
+
+
+
+
+Parse.Cloud.define("likePhoto", function(request, response) { //TODO  for increment
 	var photoId = request.params.photoId;
-	var userId = request.user.id;
 	var query = new Parse.Query("Photo");
-	query.get(photoId, {
-		  success: function(photo) {
-		    photo.addUnique("likes_id", userId);
-		    photo.remove("Reviewers", userId);
-		    photo.increment("sendCount");
-		    photo.increment("likes");
-		    var currentDate = new Date();
-		    photo.set("LastLikedAt", currentDate);
-		    photo.save();
-		    response.success();
-		  },
-		  error: function(object, error) {
-			response.error();
-		  }
-		});
+	var queryPick2RandomUsers = new Parse.Query("holder");
 	
+	query.notEqualTo("likes_id", request.user.id);
+	query.equalTo("Reviewers",request.user.id);
+	
+	Parse.Promise.when(queryPick2RandomUsers.get("tsaSJggSgZ"),query.get(photoId))
+	.then(function(holder, photoRecieved){
+		
+		
+		var min = 1; //for further improvements
+	    var userCount =  holder.get("userCount");
+		var max = userCount;
+	    var userCount1 = Math.floor(Math.random() * (max - min + 1)) + min;
+	    var userCount2 = 1; // initialized for crash protection. TODO - deprecate
+	    for (var i = 0; i < 100; i++) { //TODO random using [0;n) (n;max] where n  - is userCount1
+			userCount2 = Math.floor(Math.random() * (max - min + 1)) + min;
+			if (userCount1 != userCount2) {
+				break;
+			}
+		}
+	    newReviewers =[userCount1,userCount2];
+ 
+		var userId = request.user.id;
+		var userNum = request.user.get("userNumber");
+		var currentDate = new Date();
+		
+		photoRecieved.set("LastLikedAt", currentDate);
+		
+		photoRecieved.addUnique("likes_id", userId);
+		photoRecieved.addUnique("likes_id_num", userNum);
+		photoRecieved.addUnique("Reviewers_num", newReviewers[0]); //indian code alert! TODO
+		photoRecieved.addUnique("Reviewers_num", newReviewers[1]);
+	    
+		photoRecieved.increment("sendCount");
+		photoRecieved.increment("likes");
+		Parse.Cloud.run("sendPushForPhotoNewReviewers", newReviewers);
+		var photo = photoRecieved; // TODO - get rid of this
+		photoRecieved.save().then(writeReviwersIds(photo, newReviewers));
+	});
+	
+});
+
+Parse.Cloud.define("sendPushForPhotoObject", function(request) {
+	var userQuery = new Parse.Query(Parse.User);
+	userQuery.containedIn("userNumber", request.get("Reviewers"));
+	
+	// Find devices associated with these users
+	var pushQuery = new Parse.Query(Parse.Installation);
+	pushQuery.matchesQuery("user", userQuery);
+	
+	// Send push notification to query
+	Parse.Push.send({
+		where: pushQuery,
+		data: {
+			alert: "New Rando to review!"
+		}, 
+			  success: function() {
+				    // Push was successful
+				  },
+				  error: function(error) {
+				    // Handle error
+				  }
+		
+	});
+
+});
+
+Parse.Cloud.define("sendPushForPhotoNewReviewers", function(request) {
+	var userQuery = new Parse.Query(Parse.User);
+	userQuery.containedIn("userNumber", request);
+	
+	// Find devices associated with these users
+	var pushQuery = new Parse.Query(Parse.Installation);
+	pushQuery.matchesQuery("user", userQuery);
+	
+	// Send push notification to query
+	Parse.Push.send({
+		where: pushQuery,
+		data: {
+			alert: "New Rando to review!"
+		}, 
+			  success: function() {
+				    // Push was successful
+				  },
+				  error: function(error) {
+				    // Handle error
+				  } 
+		
+	});
+
 });
 
 Parse.Cloud.afterSave("Photo", function(request) {
-if(request.object.existed() == false) {	
-	var queryCount = new Parse.Query(Parse.User);
-	queryCount.ascending("createdAt");
-	queryCount.notContainedIn("id", [request.object.get("createdBy")]);
-	queryCount.count({
-	  success: function(count) {
-		    // The count request succeeded. Pick two random numbers
-		  	var min = 0;
-		  	var max = count;
-		    var userIndex1 = Math.floor(Math.random() * (max - min + 1)) + min;
-		    var userIndex2 = 1;
-		    for (var i = 0; i < 100; i++) {
-				userIndex2 = Math.floor(Math.random() * (max - min + 1)) + min;
-				if (userIndex1 != userIndex2) {
-					break;
-				}
-			}
-		    
-		    queryCount.skip(userIndex1-1);
-		    queryCount.limit(1);
-		    queryCount.find({
-		    	  success: function(users) {
-		    		  	  var userId1 = new String();
-		    		      userId1 = users[0].id;
-		    		      queryCount.skip(userIndex2-1);
-		    		      queryCount.find({
-		    		    	  success: function(users) {
-		    		    		  var userId2 = new String();
-		    		    		  userId2 = users[0].id; 
-		    		    		  var reviewersList = new Array( userId1, userId2);
-		    		    		  request.object.set("Reviewers", reviewersList);
-		    		  		      var currentDate = new Date();
-		    		  		      request.object.set("LastLikedAt", currentDate);
-		    		    		  request.object.save();
-		    		    	  }
-		    		    	});
-		    		  }
-		    		});
-		  },
-		  error: function(error) {
-		    // The request failed
-		  }
-		});
-}
+	if(request.object.existed() == false) {
+		var query = new Parse.Query("holder"); //TODO - make this thru pick2RandomUsersSimple
+ 		query.get("tsaSJggSgZ", { //This is holder for last user number
+ 			  success: function(holder) {
+ 				var min = 1; //for further improvements
+ 			    var userCount =  holder.get("userCount");
+ 				var max = userCount;
+ 			    var userCount1 = Math.floor(Math.random() * (max - min + 1)) + min;
+ 			    var userCount2 = 1; // initialized for crash protection. TODO - deprecate
+ 			    for (var i = 0; i < 100; i++) { //TODO random using [0;n) (n;max] where n  - is userCount1
+ 					userCount2 = Math.floor(Math.random() * (max - min + 1)) + min;
+ 					if (userCount1 != userCount2) {
+ 						break;
+ 					}
+ 				}
+ 			    var userArray =[userCount1,userCount2];
+ 			    
+ 				 var photo = request.object;
+ 			 		photo.set("Reviewers_num", userArray); // hope result is array
+ 			 		
+ 			 		photo.save().then(writeReviwersIds(photo, userArray));
+ 			 		
+ 			 		//Parse.Cloud.run("writeReviwersIds", {photo:photo,array:userArray});
+ 			 		Parse.Cloud.run("sendPushForPhotoNewReviewers", userArray);
+ 			 		
+ 				}
+ 		}); 
+	}	
+});
 
-	
-	if(request.object.existed()==true) { 
-	var reviewersList = request.object.get("Reviewers");
-	for (var i = 0; i < reviewersList.length; i++) {
-		  Parse.Push.send({
-			  channels: [ "user_"+reviewersList[i] ],
-			  data: {
-			    alert: "New Rando to review!"
-			  }
-			}, {
-			  success: function() {
-			    // Push was successful
-			  },
-			  error: function(error) {
-			    // Handle error
-			  }
-			});
-		}
-	  }
+Parse.Cloud.afterSave(Parse.User, function(request) {
+	if(request.object.existed() == false) {
+		var query = new Parse.Query("holder");
+		query.get("tsaSJggSgZ", { //This is holder for last user number
+			  success: function(holder) {
+			    var userCount =  holder.get("userCount");
+			    userCount = userCount+1;
+			    request.object.set("userNumber", userCount);
+			    holder.increment("userCount");
+			    holder.save();
+			    request.object.save();
+				}
+		});
+	}
+});
+
+function writeReviwersIds(photo, array) {
+	var userQuery = new Parse.Query(Parse.User);
+	userQuery.containedIn("userNumber", array);
+	userQuery.find({
+		success:function(list){
+			for (var i = 0; i < list.length; i++) {
+				var userId = list[i].id;
+				photo.add("Reviewers",userId);
+			}
+			photo.save();
+		}	
 	});
+};
+
+function removeOldReviewer(photo, id, num){
+    photo.remove("Reviewers", id);
+    photo.remove("Reviewers_num", num);
+    photo.save();
+}
 
 Parse.Cloud.job("getRidOfRandos", function(request, status) {
 	  // Set up to modify user data
@@ -166,3 +220,4 @@ Parse.Cloud.job("getRidOfRandos", function(request, status) {
 	    status.error("Error delelting not liked photos.");
 	  });
 	});
+
